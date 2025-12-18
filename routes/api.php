5f7397818f2b4f2b4445a49acc1addd451ae73e6
dujuu <juan.meneses75m@gmail.com>
2025-12-18 00:31:07 -0300

    a

--- routes/api.php
+++ routes/api.php
@@ -21,6 +21,7 @@ use App\Http\Controllers\UsuarioController;
 use App\Http\Controllers\Prestamo\DevolucionAdminController;
 use App\Http\Controllers\ReportesController;
 use App\Http\Controllers\reportes\Dashboard\DashboardReportesController;
+use App\Http\Controllers\Prestamo\AdminPrestamoController;
 
 
 /*
@@ -148,6 +149,12 @@ Route::post('/equipos/relacion', [EquipoRelacionadoController::class, 'store']);
 Route::delete('/equipos/relacion', [EquipoRelacionadoController::class, 'destroy']);
 Route::get('/equipos/{id}/recomendaciones', [EquipoRelacionadoController::class, 'recomendaciones']);
 
+/*
+|--------------------------------------------------------------------------
+| RUTAS ADMINISTRATIVAS
+|--------------------------------------------------------------------------
+*/
+
 // ---------------------------------------------------------------
 // Registro de ruta para la creación de equipos
 // ---------------------------------------------------------------
@@ -166,3 +173,12 @@ Route::prefix('reportes/dashboard')->group(function () {
     Route::get('/sanciones-rechazos', [DashboardReportesController::class, 'getSancionesYRechazos']);
     Route::get('/top-alumnos', [DashboardReportesController::class, 'getTopAlumnos']);
 });
+
+Route::middleware(['auth:sanctum', 'role:admin'])
+    ->prefix('admin')
+    ->group(function () {
+
+        // Registrar préstamo directamente (ADMIN)
+        Route::post('/prestamos', [AdminPrestamoController::class, 'store']);
+
+    });
